31f6a75e7592dd07c7300c3cd5ace01556d4df1b
ManimalaMariappan <37343466+ManimalaMariappan@users.noreply.github.com>
2018-03-13 10:33:13 -0700

    Update f22project.py

--- f22project.py
+++ f22project.py
@@ -1,29 +1,27 @@
-//CODE FOR THE TEST PROJECT BY F22 LABS//
-
-from urllib.request import urlopen                 //Module to retreive data from url
-import json, urllib.request                        //Module to parse json data
-url = input("Enter the url: \n")                   //getting url through input function and storing in a variable
-resp = urllib.request.urlopen(url)                 //open the url through urlopen
-data = json.loads(resp.read().decode())            //Decoding the json data
-invest = set([])                                   //creating an empty set
+from urllib.request import urlopen                 
+import json, urllib.request                      
+url = input("Enter the url: \n")                   
+resp = urllib.request.urlopen(url)                 
+data = json.loads(resp.read().decode())          
+invest = set([])                                 
 try:
     for i in range(1,len(data)+1):
         for x in data['Episode '+str(i)]:
-            l = x['investors'].replace('and',',')      //Replacing 'and' by comma in the investors names
-            l=l.split(',')                             //splitting the names individually which contains comma
+            l = x['investors'].replace('and',',')      
+            l=l.split(',')                             
             for i in range(len(l)):
-                l[i] = l[i].strip('\n').lstrip().rstrip()   //Removing left and right side spaces and '\n' in the data
+                l[i] = l[i].strip('\n').lstrip().rstrip()   
             invest = invest.union(l)
     invest.remove('')
-    oo = {e:[] for e in invest}                       //Creating array
+    oo = {e:[] for e in invest}                       
     for i in range(1,len(data)+1):
         for x in data['Episode '+str(i)]:
             sample = x['investors']
             for i in oo:
                 if i in sample:
-                    oo[i].append(x['product'].lstrip().rstrip().strip('\n'))      //Adding investors names with products  
-    for k in sorted(oo, key=lambda k: len(oo[k]), reverse=True):              //Sorting the array using its length
-        print(str(k)+" : "+str(oo[k]))                                       //Printing the array
+                    oo[i].append(x['product'].lstrip().rstrip().strip('\n'))      
+    for k in sorted(oo, key=lambda k: len(oo[k]), reverse=True):              
+        print(str(k)+" : "+str(oo[k]))                                 
         print("\n")
 except(ValueError, KeyError, TypeError):
     print("Error in JSON format")
